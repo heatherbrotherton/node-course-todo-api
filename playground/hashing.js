848aee4f787d73aba4bba8d89d306ec0973c0955
Heather Brotherton <heather.brotherton@salesforce.com>
2018-09-27 10:26:13 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,21 +1,37 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$k0UNWA6/I1xVe7DifDr/rem.o1gaeYb7pXONXBloWyntXf.YUJ3MK';
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+//
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
 
 //In library JSON webtoken
 // var data = {
