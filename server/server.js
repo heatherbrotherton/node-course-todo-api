9c6deee02e5288f1eae54a982490fb0e6873f49b
Heather Brotherton <heather.brotherton@salesforce.com>
2018-09-28 16:11:55 -0400

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -142,7 +142,13 @@ app.post('/users', (req, res) => {
     //res.send(body);
   });
 
-
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
